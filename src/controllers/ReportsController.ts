053236bde454be0b51eeec571003d6f8dd84f1d2
RafaScripts <rafaelw@rafascripts.live>
2022-04-26 01:32:30 -0300

    Auto stash before merge of "main" and "origin/main"

--- src/controllers/ReportsController.ts
+++ src/controllers/ReportsController.ts
@@ -1,6 +1,7 @@
 import Knex from '../database/index';
 import pdf from 'html-pdf';
 
+
 class ReportsController {
     async index (req: any, res: any){
         const response = await Knex('orc').where({ status: 2 });
